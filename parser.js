9c4b8f52b0b295a3686010fdcaaf855906c8d5f9
Eri Irawan <eryirawan91@gmail.com>
2017-02-11 21:30:52 +0700

    well done

--- parser.js
+++ parser.js
@@ -2,7 +2,7 @@
 const fs = require('fs')
 
 class Person {
-  constructor(id,first_name,last_name,email,phone){
+  constructor(id, first_name, last_name, email, phone){
     this.id         = id
     this.first_name = first_name
     this.last_name  = last_name
@@ -24,6 +24,7 @@ class PersonParser {
 
     let readFile = fs.readFileSync(this._file,"utf-8").split('\n')
     this._people = []
+
     for (let i = 1; i < readFile.length; i++) {
       let data = readFile[i].split(',')
       this._people.push(new Person(data[0], data[1], data[2], data[3], data[4]))
@@ -32,28 +33,31 @@ class PersonParser {
   }
 
   addPerson(id,first_name,last_name,email,phone) {
-    this._people.push(new Person(this._people +1, first_name, last_name, email, phone))
+    let person = new Person(this._people.length +1, first_name, last_name, email, phone)
+    this._people.push(person)
+    this.save()
   }
 
   save(){
     for (let i = 0; i < this._people.length; i++) {
-      this._people[i] = `${this._people[i].id}, ${this._people[i].first_name},${this._people[i].last_name},${this._people[i].email},${this._people[i].phone},${this._people[i].created_at}`
+      this._people[i] = `${this._people[i].id}, ${this._people[i].first_name}, ${this._people[i].last_name}, ${this._people[i].email}, ${this._people[i].phone}, ${this._people[i].created_at}`
     }
+
     this._people.unshift()
     this._people = this._people.join('\n')
+
+    console.log(`There are ${parser._people.length} people in the file '${parser._file}'.`)
 
     fs.writeFileSync(this._file, this._people, 'utf-8', function(err){
       err ? console.log(err):console.log('Data has been saved !!');;
     })
   }
-
 }
 
 let parser = new PersonParser('people.csv')
     parser.people
 
 console.log(parser.people);
-console.log(`There are ${parser._people.size} people in the file '${parser._file}'.`)
 
-parser.addPerson('Eri', 'Irawan', 'email@email.com', 1234567890)
-parser.save()
+parser.addPerson('','Eri', 'Irawan', 'email@email.com', 1234567890)
+// parser.save()
