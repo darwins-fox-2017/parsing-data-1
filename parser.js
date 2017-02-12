0ae71a3f7de84cc92f7cd3d4dbb7de999ff9c248
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-02-12 15:33:26 +0700

    Danang first commit

--- parser.js
+++ parser.js
@@ -1,32 +1,88 @@
 "use strict"
+import * as fs from 'fs'
 
 class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
+  constructor(id,first_name,last_name,email,phone, created_at) {
+    this.id         = id
+    this.first_name = first_name
+    this.last_name  = last_name
+    this.email      = email
+    this.phone      = phone
+    this.created_at = created_at || new Date()
+  }
 }
 
 class PersonParser {
 
   constructor(file) {
-    this._file = file
+    this._file   = file
     this._people = null
   }
 
   get people() {
-    // If we've already parsed the CSV file, don't parse it again
-    // Remember: people is null by default
     if (this._people)
       return this._people
 
-    // We've never called people before, now parse the CSV file
-    // and return an Array of Person objects here
-    // Save the Array in the people instance variable.
+    this.readPeople()
+    return this._people
   }
 
-  addPerson() {}
+  add (first_name, last_name, email, phone) {
+    let newPerson = new Person(this._people.length+1, first_name, last_name, email, phone)
+    this._people.push(newPerson)
+    this.save()
+  }
+
+  save() {
+    let people = []
+    this._people.map((person) => {
+      people.push(`${person.id},${person.first_name},${person.last_name},${person.email},${person.phone},${person.created_at}`)
+    })
+    
+    people.unshift(`id,first_name,last_name,email,phone,created_at`)
+    this._people = people.join('\n')
+
+    fs.writeFileSync(this._file, this._people, "utf-8", (err) => {
+      err ? console.log(err) : console.log('Berhasil disimpan');
+    })
+  }
+
+  readPeople() {
+    this._people = []
+    this.readData((row, col) => {
+      this._people.push(new Person(col(1), col(2), col(3), col(4), col(5), col(6)))
+    })
+  }
+
+  getCsv() {
+    return fs.readFileSync(this._file, 'utf-8')
+  }
+
+  getRows() {
+    return this.getCsv().split('\n')
+  }
+
+  getRowData(row) {
+    return this.getRows()[row]
+  }
+
+  getColumnData(row, col) {
+    return (col) => {
+      return this.getRowData(row).split(',')[col-1]
+    }
+  }
+
+  readData(callback) {
+    for (let i = 1; i < this.getRows().length; i++) {
+      let getCol = this.getColumnData(i)
+      callback(this.getRowData(i), getCol, i)
+    }
+  }
 
 }
 
 let parser = new PersonParser('people.csv')
 
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+parser.readPeople()
+parser.add('Danang', 'Aji Tamtomo', 'imsuretamtomo@gmail.com', '085846020358')
+console.log(parser.people)
