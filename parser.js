29a7046bae06f3830b3653da4e08e47913536793
danilags <aguspermadi94@gmail.com>
2017-02-14 19:07:35 +0700

    update parsing

--- parser.js
+++ parser.js
@@ -1,13 +1,13 @@
 "use strict"
-const fs = require("fs")
+const fs = require('fs')
 class Person {
-  constructor(id, firstName, lastName, email, phone) {
+  constructor(id, first_name, last_name, email, phone) {
     this.id = id
-    this.firstName = firstName
-    this.lastName = lastName
+    this.first_name = first_name
+    this.last_name = last_name
     this.email = email
     this.phone = phone
-    this.create_at = new Date()
+    this.created_at = new Date()
   }
   // Look at the above CSV file
   // What attributes should a Person object have?
@@ -24,44 +24,53 @@ class PersonParser {
   get people() {
     // If we've already parsed the CSV file, don't parse it again
     // Remember: people is null by default
-    if (this._people) {
+    if (this._people){
       return this._people
     }
+    console.log('executed');
+    let data = fs.readFileSync(this._file, 'utf-8').split('\n')
+    this._people = []
+    for (let i = 1; i < data.length; i++) {
+      let dataSplit = data[i].split(',')
+      this._people.push(new Person(dataSplit[0],dataSplit[1],dataSplit[2],dataSplit[3],dataSplit[4] ))
+    }
+    return this._people
+
 
     // We've never called people before, now parse the CSV file
     // and return an Array of Person objects here
     // Save the Array in the people instance variable.
-    let data = fs.readFileSync(this._file, 'utf-8').split('\n')
-    this._people = []
-    for(let i = 1; i < data.length; i++) {
-      let dataparse = data[i].split(',')
-      this.people.push(new Person(dataparse[0], dataparse[1], dataparse[2], dataparse[3], dataparse[4]))
-    }
-    return this._people
+
   }
 
-  addPerson(firstName, lastName, email, phone, create_at) {
-    this._people.push(new Person (this._people.length+1, firstName, lastName, email, phone))
+  addPerson(first_name,last_name,email,phone, created_at) {
+    this._people.push(new Person (this._people.length+1,first_name,last_name,email,phone ))
   }
 
   submit() {
-    for(let i = 0; i < this._people.length; i++) {
-      this._people[i] = `${this._people[i].id}, ${this._people[i].firstName}, ${this._people[i].lastName},
-      ${this._people[i].email}, ${this._people[i].phone}, ${this._people[i].create_at}`
+    for (let i = 0; i < this._people.length; i++) {
+      this._people[i] = `${this._people[i].id},${this._people[i].first_name},${this._people[i].last_name},${this._people[i].email},${this._people[i].phone},${this._people[i].created_at}`
     }
-    this._people.unshift()
-    this._people = this._people.join('\n')
+    
+    let dataCSV = "id,first_name,last_name,email,phone,created_at\n"
+    dataCSV += this._people.join('\n')
 
-    fs.writeFile(this._file, this._people, 'utf-8', function(err) {
-      err ? console.log(err) : console.log(`Data disimpan`)
+    fs.writeFile(this._file,dataCSV,'utf-8', function(err) {
+    
+      if(err) {
+        console.log(err);
+      } else {
+        console.log('Data berhasil disimpan !');
+      }
     })
   }
 
 }
 
 let parser = new PersonParser('people.csv')
+
 parser.people
-parser.addPerson('Samuel', 'sinaga', 'samuel@gmail.co.id', 0822766775542)
-parser.addPerson('Daniel', 'agus', 'samuel@gmail.co.id', 0822766775542)
-//console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+parser.addPerson('daniel', 'agus', 'agus@gmail.com', 087765245272)
+parser.addPerson('wahyu', 'yulia', 'wahyu@gmail.com', 087765245272)
+parser.addPerson('Michael', 'sam', 'todo@gmail.com', 08297528292)
 parser.submit()
